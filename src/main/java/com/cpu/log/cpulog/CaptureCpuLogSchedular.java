e40685970c1bc5786661144c58eab76bda809897
vikasbelote <vikasb0207@gmail.com>
2019-07-18 04:05:21 +0530

    commit

---
+++ src/main/java/com/cpu/log/cpulog/CaptureCpuLogSchedular.java
@@ -0,0 +1,23 @@
+package com.cpu.log.cpulog;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import com.cpu.log.utils.CaptureCpuLog;
+
+@Component
+public class CaptureCpuLogSchedular {
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+	@Scheduled(fixedRate = 5000)
+	public void reportCurrentTime() {
+		
+		System.out.println("The time is now " + dateFormat.format(new Date()));
+		CaptureCpuLog captureCpuLog = new CaptureCpuLog(true);
+		captureCpuLog.createLog();
+	}
+}
